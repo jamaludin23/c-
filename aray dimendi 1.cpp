83b212d3d8e90e7f4dd91244c5ac0f0fc27d3687
jamaludin23 <jamaludi12161254@bsi.ac.id>
2016-12-07 16:32:48 +0700

    Add files via upload

---
+++ aray dimendi 1.cpp
@@ -0,0 +1,15 @@
+#include <conio.h>
+#include <iostream.h>
+
+//inisialisasi aray dimensi 1
+main()
+{
+
+char hari[7][10]={"minggu","senin","selasa","rabu","kamis","jum'at","sabtu"};
+
+clrscr();
+cout<<"nama-namahari:"<<endl;
+cout<<hari[0]<<endl<<hari[1]<<endl<<hari[2]<<endl<<hari[3]<<endl<<hari[4]<<endl<<hari[5]<<endl<<hari[6]<<endl<<hari[7]<<endl;
+
+getch();
+}
