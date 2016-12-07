cc4bdc20fef571b5143c990b1321f43fdd08e776
jamaludin23 <jamaludi12161254@bsi.ac.id>
2016-12-07 16:30:26 +0700

    Add files via upload

---
+++ IF (NILAI TUGAS).cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream.h>
+main()
+{
+cout<<" \t\t\t\tNILAI TUGAS C++"<<endl;
+int nilai,tugas;
+
+printf("Masukan Sebuah Nilai:");scanf("%d",&nilai);
+printf("Masukan Nilai Tugas:");scanf("%d",&tugas);
+if(nilai>70)
+printf("\t\t\t\tLULUS\n");
+else
+printf("Tidak Lulus\n");
+getch();
+}
